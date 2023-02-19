89072335a8d31c4e3401965753716ebe8775d53b
Al Hussein KHOUMA <khouma964@gmail.com>
2023-02-19 18:44:07 +0000

    condig database

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
+// const url = "mongodb://localhost:27017/mern-project";
+const url =
+  "mongodb+srv://moustaphakhouma964:zP8kFpa4ScC33v5L@mydata.9wncuz2.mongodb.net/parlons-de-nous";
+const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/mern-project"
-const mongoose = require('mongoose');
-
-mongoose.connect(url,
-  {
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
     // useFindAndModify: false
   })
-  .then(() => console.log('connected to mongo db'))
-  .catch((err) => console.log('Failed to connect to mongo ' + err))
+  .then(() => console.log("connected to mongo db"))
+  .catch((err) => console.log("Failed to connect to mongo " + err));
