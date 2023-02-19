aba70988455c38668109ff04b9ac9d2ec7a57ebe
Al Hussein KHOUMA <khouma964@gmail.com>
2023-02-19 19:53:10 +0000

    add config

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Max-Age", "1000");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token,Origin,X-Requested-With,Content,Accept-Version"
+    " X-Requested-With,content-type"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
